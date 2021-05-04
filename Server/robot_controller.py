44cffaf4f098d432a0010360345812405587d8d8
JinSakuma <jinskm2017@gmail.com>
2021-05-04 15:37:08 +0900

    modified mmdagent method

--- Server/robot_controller.py
+++ Server/robot_controller.py
@@ -15,7 +15,6 @@ import threading
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append (os.path.abspath(os.path.join(current_dir, '../MMDAgentClient', 'run')))
-print(os.path.abspath(os.path.join(current_dir, '../MMDAgentClient', 'run')))
 import run_action_player
 
 os.environ["REGISTRY_SERVER_PORT"] = "25001"
